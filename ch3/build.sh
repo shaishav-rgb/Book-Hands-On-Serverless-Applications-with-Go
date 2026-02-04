6c3abeab2cf66da136484ea29951213b313fd7d7
shaishav-rgb <shaishavpaudel@gmail.com>
2026-02-04 05:49:35 +0545

    feat: add lambda deployment script

--- ch3/build.sh
+++ ch3/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Build the binary"
-GOOS=linux GOARCH=amd64 go build -o main main.go
+GOOS=linux GOARCH=amd64 go build -o bootstrap main.go
 
 echo "Create a zip file"
-zip deployment.zip main
+zip deployment.zip bootstrap
 
 echo "Cleaning up"
-rm main
+rm bootstrap
--- ch3/main.go
+++ ch3/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/aws/aws-lambda-go/lambda"
 
 func handler() (string, error) {
-	return "Welcome to Serverless world", nil
+	return "Welcome to Serverless world uploaded from s3", nil
 }
 
 func main() {
